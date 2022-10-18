01cfca5d3b7f5f3df270ce8d93d61df4c47f41be
Gopal Gautam <104998098+GOPALGTM@users.noreply.github.com>
2022-10-18 13:08:09 +0530

    Create after_install.sh

---
+++ after_install.sh
@@ -0,0 +1,10 @@
+#1bin/bash
+
+sudo mkdir project
+cd project
+virtualenv env
+source env/bin/activate
+cd Authentication-System
+pip install django gunicorn
+sudo ufw allow 8000
+python manage.py runserver
