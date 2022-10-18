128258ad46750d5d59974f70889ebcf2b4225916
Gopal Gautam <104998098+GOPALGTM@users.noreply.github.com>
2022-10-18 15:12:30 +0530

    Update before_install.sh

--- before_install.sh
+++ before_install.sh
@@ -3,3 +3,7 @@ sudo apt-get update
 sudo apt-get install python3-pip python3-dev libpq-dev nginx curl -y
 sudo -H pip install --upgrade pip
 sudo -H pip install virtualenv
+sudo mkdir project
+cd project
+virtualenv env
+source env/bin/activate
