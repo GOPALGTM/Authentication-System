e47433f2a25c22d004418d2ad9f5a65ff21cd3e8
Gopal Gautam <104998098+GOPALGTM@users.noreply.github.com>
2023-01-07 13:47:17 +0530

    Update after_install.sh

--- after_install.sh
+++ after_install.sh
@@ -1,5 +1,7 @@
 #1bin/bash
-cd /home/ubuntu/project
-pip install django gunicorn
+virtualenv -p python3 venv
+source venv/bin/activate
+pip install -r requirements.txt
+python manage.py migrate
 sudo ufw allow 8000
-
+python3 manage.py runserver 0.0.0.0:8000
