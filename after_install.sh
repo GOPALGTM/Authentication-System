8e900ad2ea8b6476d7ec205a5e0289315c29cbc4
Gopal Gautam <104998098+GOPALGTM@users.noreply.github.com>
2023-01-07 14:14:15 +0530

    Update after_install.sh

--- after_install.sh
+++ after_install.sh
@@ -2,6 +2,6 @@
 virtualenv -p python3 venv
 source venv/bin/activate
 pip install -r requirements.txt
-python manage.py migrate
+python3 manage.py migrate
 sudo ufw allow 8000
 python3 manage.py runserver 0.0.0.0:8000
